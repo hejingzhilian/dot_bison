07944751bf7ea9f97c5f0355bbb7a794d22cbab9
shishuai <shishuai@.qq.com>
2023-05-29 12:22:50 +0000

    add scanner test

--- syntax/test/dot_scanner_test.cc
+++ syntax/test/dot_scanner_test.cc
@@ -1,3 +1,26 @@
 #include"gtest/gtest.h"
+#include "syntax/DotScanner.h"
+
+
+class DotScannerTest:public ::testing::Test {
+public:
+    DotScannerTest() {
+        std::cout << " init Dot DotScannerTest" << std::endl;
+    }
+protected:
+    int i;
+};
+
+
+TEST_F(DotScannerTest, yylexTest) {
+    
+}
+
+
+int main(int argc , char * argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
 
 
